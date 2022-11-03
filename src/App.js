7b185cf76e0f30d32afb833f78fd02f454e337cf
rexdev08 <raimundorincong@gmail.com>
2022-11-03 14:34:33 -0300

    agregue texto de demostracion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Editor from './components/Editor.jsx';
 import Previewer from './components/Previewer.jsx';
@@ -7,7 +7,34 @@ import Previewer from './components/Previewer.jsx';
 
 function App() {
 
+  useEffect(()=>{
+   setContent(`### Hola, soy Raimundo Rincon 👋🏻👨🏻‍💻 
+
+   ![alt text](https://www.codewars.com/users/rexdev08/badges/large)
+   
+   💼 [Portafolio](https://rexdev08.github.io/)
+   
+   🔷 [Twitter](https://twitter.com/RexDev08)
+   
+   ◽ [Linkedin](https://www.linkedin.com/in/rexdev08/)
+
+   🐈‍⬛ [Github](https://github.com/rexdev08)
+   
+   
+   Desarrollador web | HTML | CSS | Javascript | SCSS | ReactJS
+   
+   Soy un apasionado de la programación, con buenos valores, tengo soft skills (soy amable y respetuoso...) y tambien dispuesto a trabajar con nuevas tecnologias.
+   
+   Una de mis mayores motivaciones es aprender. Así empecé haciendo cursos, devorando libros , tutoriales y ademas complemento lo anterior siendo autodidacta.
+   
+   ![coding](https://user-images.githubusercontent.com/101665808/195746743-c3f64ae4-34ae-46f0-85d6-1aaffb506734.gif)
+   
+   `)
+  },[])
+
 const [content, setContent] = useState("")
+
+
  
   const text = (e) =>{
       setContent(e.currentTarget.value)
@@ -15,8 +42,8 @@ const [content, setContent] = useState("")
 
   return (
     <div className="App">
-      <Editor onChangeHandler={text} />
-      <Previewer elements={content}/>
+      <Editor start={content} onChangeHandler={text} />
+      <Previewer elements={content} />
     </div>
   );
 }
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,17 +2,17 @@ import styles from "../styles/Editor.module.scss"
 import {FaExpandArrowsAlt} from "react-icons/fa"
 import {FaCompressArrowsAlt} from "react-icons/fa"
 
-const Editor = ({onChangeHandler}) => {
+const Editor = ({start, onChangeHandler}) => {
 
 return (<div id="editor" className={styles.editor}>
     <header className={styles.editor__header}>
-        <span className={styles.editor__span}>Editor</span>
+        <span className={styles.editor__span}>Markdown Editor</span>
         <button className={styles.editor__button}>
             <FaExpandArrowsAlt className={styles.button__expand}/>
             <FaCompressArrowsAlt className={styles.button__compress}/>
         </button>
     </header>
-    <textarea onChange={onChangeHandler} className={styles.editor__text} name="info" id="info" cols="30" rows="10"></textarea>
+    <textarea value={start} onChange={onChangeHandler} className={styles.editor__text} name="info" id="info" cols="30" rows="10"></textarea>
 </div>)
 
 
